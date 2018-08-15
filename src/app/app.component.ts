83871be189b263cc92de7c0c915f839c15ddfe2f
Derek Hammer and Eddie Harris <eddie@>
2018-08-15 11:34:37 -0700

    initial commit with basic framework and constructor and some items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Brew } from './models/brew.model'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Grand Wizard Eddies Potion Brewing Hut';
+
+  potions: Brew[] = [
+    new Brew("Dragon Tooth Tea", "Health", 75, 300),
+    new Brew("Titan Grip", "Strength", 45, 150),
+    new Brew("Mana Wraith Essence", "Mana", 50, 200),
+    new Brew("Giant's Toe Ale", "Stamina", 25, 100)
+  ]
+
+
+
+  editTask(potList){
+    this.potions = null;
+  }
+
+
+
+  textColor(potList){
+    if (potList.type === "Health"){
+      return "text-success";
+    }else if (potList.type === "Strength"){
+      return "text-danger";
+    }else if (potList.type === "Mana"){
+      return "text-primary";
+    }else if(potList.type === "Stamina"){
+      return "text-warning";
+    }
+
+  }
 }
---
+++ src/app/models/brew.model.ts
@@ -0,0 +1,4 @@
+export class Brew{
+  amount: number = 125;
+  constructor(public name: string, public type: string, public potency: number, public cost: number) {}
+}
